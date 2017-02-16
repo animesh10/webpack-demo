82dd917bdade3dcc6145861bac030c8050c0a1fa
Animesh Dutta <a0d00hf@m-C02S32CGG8WM.local>
2017-02-16 14:16:55 -0800

    migrate loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var ReactToHtmlPlugin = require('react-to-html-webpack-plugin');
-
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 var path = require('path');
 var ejs = require('ejs');
 var fs = require('fs');
 
+let cssLoaderQuery = 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader';
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'main' : './src/index.js'
+  },
 
   output: {
     filename: 'index.js',
@@ -16,27 +18,43 @@ module.exports = {
   },
 
   module: {
-    loaders: [
+    rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
-      { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: cssLoaderQuery }) },
+      { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function() {
+                return [
+                  require('autoprefixer'),
+                  require('postcss-color-rebeccapurple-loader')
+                ];
+              }
+            }
+          }
+        ]
+      }
     ]
   },
 
-  postcss: [
-    require('autoprefixer-core'),
-    require('postcss-color-rebeccapurple')
-  ],
+  // postcss: [
+  //   require('autoprefixer'),
+  //   require('postcss-color-rebeccapurple')
+  // ],
 
   resolve: {
-    modulesDirectories: ['node_modules', 'components']
+    modules: ['node_modules', 'components']
   },
 
   plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true }),
-    new ReactToHtmlPlugin('index.html', 'index.js', {
-      static: true,
-      template: ejs.compile(fs.readFileSync(__dirname + '/src/template.ejs', 'utf-8'))
+    new ExtractTextPlugin({ filename: 'style.css', disable: false, allChunks: true }),
+    new StaticSiteGeneratorPlugin('main', '/', {
+      //static: true,
+      //template: ejs.compile(fs.readFileSync(__dirname + '/src/template.ejs', 'utf-8'))
     })
   ]
 };
